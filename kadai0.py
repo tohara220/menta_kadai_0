dc93fbeafcf516c3a45651add1918f2807db6c21
tohara220 <tohara.220@gmail.com>
2022-01-03 17:02:43 +0900

    firstcommit

---
+++ kadai0.py
@@ -0,0 +1,15 @@
+character_list = ["たんじろう","ぎゆう","ねずこ","むざん"]
+character_list.append("ぜんいつ")
+
+# for文で回して、キャラクター名を表示させる
+for name in character_list:
+    print(name)
+
+# 関数でキャラクターの存否を判定する
+def test(character_name):
+    if character_name in character_list:
+        print(f"{character_name}は存在します。")
+    else:
+        print(f"{character_name}は存在しません。")
+
+test("ぜんいつ")
